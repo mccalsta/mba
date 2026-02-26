a20a39ec6d57972f39349afcebe4a717b3a8ae80
Henry K Atuhairwe <43408901+mccalsta@users.noreply.github.com>
2026-02-26 13:10:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-print("LOADED APP VERSION 3 - RECEIPT ROUTE ACTIVE")
+import os
+print("RUNNING FILE:", __file__)
+print("WORKING DIR:", os.getcwd())
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import sqlite3
 import os
